b1c28aba7fbabceadf4fb72b62bdd7ca99153777
bbKubek <bbkubek@wp.pl>
2022-07-06 21:21:34 +0200

    404

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', include('website.urls')),
     path('admin/', admin.site.urls),
 ]
-
+admin.AdminSite.site_title = "Website Administration"
+admin.AdminSite.site_header = "Website Administration"
+admin.AdminSite.index_title = "Website Administration"
 handler404 = 'website.views.view_page_not_found'
